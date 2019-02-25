a4bbeffc05f9d8dbde9165d649a5a76ddb3cb01d
Judy McGoldrick <jjeanius@verizon.net>
2019-02-25 01:49:46 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
 
   def create
     if params[:name]="" || params[:name]=nil
+      binding.pry
       !session[:name] = !params[:name]
       redirect_to '/sessions/new'
     elsif
